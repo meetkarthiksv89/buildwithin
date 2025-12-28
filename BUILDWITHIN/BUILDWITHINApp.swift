9c975c814754877bb4e1dd645b4925793a4b49fd
Karthik Venkatesh <meetkarthiksv89@gmail.com>
2025-12-27 16:59:16 -0800

    Initial Commit

---
+++ BUILDWITHIN/BUILDWITHINApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUILDWITHINApp.swift
+//  BUILDWITHIN
+//
+//  Created by Karthik Venkatesh on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BUILDWITHINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
