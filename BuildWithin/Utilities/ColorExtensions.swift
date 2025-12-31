2883e47d418e82d28abcfac05c02a5e5871ffd8f
Karthik Venkatesh <meetkarthiksv89@gmail.com>
2025-12-31 02:12:36 -0800

    Update color scheme to Spotify theme

--- BuildWithin/Utilities/ColorExtensions.swift
+++ BuildWithin/Utilities/ColorExtensions.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 extension Color {
-    // Dark green theme colors matching screenshots
-    static let appBackground = Color(red: 0.1, green: 0.2, blue: 0.15)
-    static let appCardBackground = Color(red: 0.12, green: 0.22, blue: 0.17)
-    static let appPrimaryGreen = Color(red: 0.2, green: 0.8, blue: 0.4)
-    static let appLightGreen = Color(red: 0.3, green: 0.85, blue: 0.5)
-    static let appTextPrimary = Color.white
-    static let appTextSecondary = Color(red: 0.7, green: 0.7, blue: 0.7)
-    static let appInactiveGray = Color(red: 0.4, green: 0.4, blue: 0.4)
-    static let appNavBarBackground = Color(red: 0.15, green: 0.15, blue: 0.15)
+    // Spotify color scheme
+    // Background: #121212 (very dark gray/black)
+    static let appBackground = Color(red: 0.071, green: 0.071, blue: 0.071) // #121212
+    
+    // Card background: #181818 (slightly lighter dark gray)
+    static let appCardBackground = Color(red: 0.094, green: 0.094, blue: 0.094) // #181818
+    
+    // Primary green: #1DB954 (Spotify green)
+    static let appPrimaryGreen = Color(red: 0.114, green: 0.725, blue: 0.329) // #1DB954
+    
+    // Light green: slightly lighter version of Spotify green
+    static let appLightGreen = Color(red: 0.2, green: 0.8, blue: 0.45)
+    
+    // Text primary: White
+    static let appTextPrimary = Color.white // #FFFFFF
+    
+    // Text secondary: #B3B3B3 (light gray)
+    static let appTextSecondary = Color(red: 0.702, green: 0.702, blue: 0.702) // #B3B3B3
+    
+    // Inactive gray: #535353 (medium gray)
+    static let appInactiveGray = Color(red: 0.325, green: 0.325, blue: 0.325) // #535353
+    
+    // Nav bar background: #000000 (black)
+    static let appNavBarBackground = Color.black // #000000
 }
